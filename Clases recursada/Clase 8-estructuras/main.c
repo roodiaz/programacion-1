3738b20fbb59b5d37cd7900c10d2519cba157556
alumno <alumno>
2019-09-16 22:17:51 -0300

    clase 8

--- Clases recursada/Clase 8-estructuras/main.c
+++ Clases recursada/Clase 8-estructuras/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 #define TAM 5
+typedef struct
+{
+    int dia;
+    int mes;
+    int anio;
+}eFecha;
 
 typedef struct
 {
@@ -12,17 +18,18 @@ typedef struct
     int nota1;
     int nota2;
     float promedio;
+    eFecha fechaIngreso;
 
 } eAlumno;
 
 void mostrarAlumno(eAlumno vec)
 {
-    printf(" %d  %8s    %d     %c     %2d        %2d      %.2f\n",vec.legajo,vec.nombre,vec.edad,vec.sexo,vec.nota1,vec.nota2,vec.promedio);
+    printf(" %d  %8s    %d     %c     %2d        %2d      %.2f   %02d/%02d/%d\n",vec.legajo,vec.nombre,vec.edad,vec.sexo,vec.nota1,vec.nota2,vec.promedio,vec.fechaIngreso.dia,vec.fechaIngreso.mes,vec.fechaIngreso.anio);
 }
 
 void mostrarAlumnos(eAlumno vec[], int tam)
 {
-    printf("Legajo   Nombre   Edad   Sexo   Nota-1   Nota-2  Promedio\n\n");
+    printf("Legajo   Nombre   Edad   Sexo   Nota-1   Nota-2  Promedio    Fecha Ingreso\n\n");
 
     for(int i=0; i<tam; i++)
     {
@@ -32,11 +39,11 @@ void mostrarAlumnos(eAlumno vec[], int tam)
 
 int main()
 {
-    eAlumno alumnos[TAM]= { {3413,"ana",24,'f',7,9,8},
-                            {5235,"juan",21,'m',6,6,6},
-                            {1234,"lucia",19,'f',9,10,9.5},
-                            {7453,"pedro",25,'m',7,7,7},
-                            {2345,"juana",22,'f',6,7,6.5}};
+    eAlumno alumnos[TAM]= { {3413,"ana",24,'f',7,9,8,{12/06/2019}},
+                            {5235,"juan",21,'m',6,6,6,{05/10/2019}},
+                            {1234,"lucia",19,'f',9,10,9.5,{24/01/2018}},
+                            {7453,"pedro",25,'m',7,7,7,{24/06/2017}},
+                            {2345,"juana",22,'f',6,7,6.5,{29/12/2018}}};
 
 
    /* for(int i=0; i<TAM; i++)
