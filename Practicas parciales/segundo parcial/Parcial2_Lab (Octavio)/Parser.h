a18bcab4078eb7a972c4f3c82eb73b71c5bfd9d7
unknown <roabigail.diaz@gmail.com>
2019-11-12 00:19:09 -0300

    .

--- Practicas parciales/segundo parcial/Parcial2_Lab (Octavio)/Parser.h
+++
@@ -1,4 +0,0 @@
-int parser_EmployeeFromText(FILE* , LinkedList* );
-int parser_EmployeeListToText (FILE* , LinkedList* );
-FILE* openFileRead(char* , LinkedList* );
-FILE* openFileWrite(char* , LinkedList* );
--- Practicas parciales/segundo parcial/Recu_2doParcial PELICULAS/Parser.h
+++
@@ -1,5 +0,0 @@
-int parser_MoviesFromText(FILE* , LinkedList* );
-int parser_MoviesListToText (FILE*  , LinkedList* );
-FILE* openFileRead(char* , LinkedList* );
-FILE* openFileWrite(char*  , LinkedList* );
-
